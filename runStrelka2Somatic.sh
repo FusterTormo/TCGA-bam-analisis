8bc85f4a8d5e3f83d610e7b7bcd00cdd2a8eeeb9
fustertormo <francisco.fuster@irbbarcelona.org>
2018-04-10 17:34:36 +0200

    Uploaded bash script to run Strelka2 for somatic variant calling. Usage: ./runStrelka2Somatic.sh normal.bam tumor.bam output_directory

---
+++ runStrelka2Somatic.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Run Strelka2 with default parameters for tumor vs control analysis. Needs input normal bam as parameter 1, input tumor bam as parameter2, and output directory as parameter 3
+reference=~/genome_references/GRCh38.d1.vd1.fa
+strelka=~/soft/Strelka2.8.4/bin/configureStrelkaSomaticWorkflow.py
+#Color constants to print in different colors
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+#Check if the number of parameters is the expected and run Strelka if it is the case
+if (( $# == 3 )); then
+	sta=`date`
+	SECONDS=0
+
+	$strelka --normalBam $1 --tumorBam $2 --referenceFasta $reference --exome --runDir $3
+	if (( $? == 0 )); then
+		echo -e "\n\n${GREEN}Strelka-2 configured successfully.${NC} Running the analysis\n\n"
+		$3/runWorkflow.py -m local -j 8 --quiet
+		if (( $? == 0 )); then
+			echo -e "${GREEN}Strelka-2 ran successfully.${NC} Results stored in $3"
+			
+			end=`date`
+			echo -e "Strelka-2 started at $sta\nEnded at $end"
+			printf 'Elapsed time -> %dh:%dm:%ds\n' $(($SECONDS/3600)) $(($SECONDS%3600/60)) $(($SECONDS%60))
+		fi
+	else
+		echo -e >&2 "\n${RED}Execution aborted. Check below possible errors${NC}\n"
+		exit 1
+	fi
+else 
+	echo -e >&2 "\nUSAGE: runStrelka2Somatic.sh normal.bam tumor.bam output_directory\n"
+	exit 1
+fi
+
