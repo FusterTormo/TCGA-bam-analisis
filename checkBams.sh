5b7df7b4c2ae1b56a4a3271619b0d6ebf481f0f4
fustertormo <francisco.fuster@irbbarcelona.org>
2018-04-24 15:27:45 +0200

    Created a bash script to test if BRCA samples are completely and successfully downloaded. Usage: ./checkBams.sh

---
+++ checkBams.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#To download new cancers, change this line to where the manifests are, and that's all
+cd /g/strcombio/fsupek_cancer3/TCGA_bam/BRCA/
+sta=`date`
+token="/mnt/data/token.txt"
+gdc="/mnt/data/gdc-client download"
+minspace=41943040	
+for f in *; do
+	echo "Getting $f. Started at `date`"
+	cd $f
+	#Manifest has the same name as the folder, plus ".txt" extension. -n parameter should be between 30-40 to get the maximum download speed
+	$gdc -t $token -m $f.txt -d . -n 40
+	echo -e "Finished at `date`\n"
+	cd ..
+	#Check if the disk is near full
+	#space=`df -k /dev/md126 | grep /dev | awk '{print $4}'`
+	#if [ $space -le $minspace ]; then 
+#		echo -e "Disk capacity reached. Exiting\n"
+#		echo -e "Last sample downloaded: $f\n"
+#		break	
+#	fi
+done
+echo -e "getBams.sh started on $sta\n\ngetBams.sh ended on `date`"
+
