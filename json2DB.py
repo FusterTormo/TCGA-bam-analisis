6af99de4d472d4e739f590f5a9be18de74b15eea
jespinosa <jose.espinosa@irbbarcelona.org>
2019-08-14 16:58:03 +0200

    Clinical data in `json` file changed

--- json2DB.py
+++ json2DB.py
@@ -1,5 +1,5 @@
 import json
-
+import sys
 #filer = "clinical.project-TCGA-HNSC.2017-12-14T12_15_13.069787.json"
 #cType = "HNSC"
 #filer = "clinical.project-TCGA-OV.2018-01-30T06_26_17.307051.json"
@@ -32,8 +32,24 @@ import json
 #cType = 'GBM'
 #filer = 'clinical.project-TCGA-PAAD.2018-10-18.json'
 #cType = 'PAAD'
-filer = 'clinical.project-TCGA-THCA.2018-11-14.json'
-cType = 'THCA'
+#filer = 'clinical.project-TCGA-THCA.2018-11-14.json'
+#cType = 'THCA'
+# filer = 'clinical.project-TCGA-CHOL.2019-07-29.json'
+# cType = 'CHOL'
+# filer = 'clinical.project-TCGA-UCS.2019-07-31.json'
+# cType = 'UCS'
+# filer = 'clinical.project-TCGA-DLBC.2019-08-02.json'
+# cType = 'DLBC'
+# filer = 'clinical.project-TCGA-UVM.2019-08-06.json'
+# cType = 'UVM'
+# filer = 'clinical.project-TCGA-ACC.2019-08-08.json'
+# cType = 'ACC'
+# filer = 'clinical.project-TCGA-THYM.2019-08-12.json'
+# cType = 'THYM'
+filer = 'clinical.project-TCGA-PCPG.2019-08-14.json'
+cType = 'PCPG'
+# filer = 'clinical.project-TCGA-SKCM.2019-08-12.json'
+# cType = 'SKCM'
 
 
 sql = "clinical_data.sql"
@@ -44,22 +60,27 @@ with open(filer, 'r') as f :
 print "Finished reading. Got {0} cases. Extracting and converting data to SQL".format(len(j))
 
 for l in j :
-    #print l['case_id'] # Alternative to Primary key
-    #print l['demographic']['submitter_id'].split('_')[0] #PRIMARY KEY
-    #print l['diagnoses'][0]['age_at_diagnosis'] #Float. Days after birth -> transform to years
-    #print type(l['diagnoses'][0]['days_to_death']) #Float. Days to death. None if still alive?
-    #print l['demographic']['gender'] #Either 'female' or 'male'
-    #print l['demographic']['race'] #String with the race. Longest is 32 characters
-    #print l['demographic']['ethnicity'] #String. Longest is 22 characters
-    #print l['demographic']['year_of_birth'] #Integer or None
-    #print l['demographic']['year_of_death'] #Integer or None
-    #print l['exposures'][0]['weight'] #Integer ?
-    #print l['exposures'][0]['height'] #Integer ?
-    #print l['exposures'][0]['bmi'] #Float / Integer ?
-    #print l['exposures'][0]['years_smoked'] # Float, maybe it is possible to convert it to int
-    #print l['exposures'][0]['cigarettes_per_day'] # Float or None
-    #print l['exposures'][0]['alcohol_history'] # String: 'yes' or 'no'
-    #print l['exposures'][0]['alcohol_intensity'] #Integer ??
+    # print "case_id------------", l['case_id'] # Alternative to Primary key
+    # print "submitter_id-------", l['demographic']['submitter_id'].split('_')[0] #PRIMARY KEY
+    # print "age_at_diagnosis---", l['diagnoses'][0]['age_at_diagnosis'] #Float. Days after birth -> transform to years
+    # try:
+    #     days_to_death = l['demographic']['days_to_death']
+    # except KeyError:
+    #     days_to_death = None
+    # print "days_to_death------", type(days_to_death) #Float. Days to death. None if still alive?
+    # print "gender-------------", l['demographic']['gender'] #Either 'female' or 'male'
+    # print "race---------------", l['demographic']['race'] #String with the race. Longest is 32 characters
+    # print "ethnicity----------", l['demographic']['ethnicity'] #String. Longest is 22 characters
+    # print "year_of_birth------", l['demographic']['year_of_birth'] #Integer or None
+    # print "year of death------", l['demographic']['year_of_death'] #Integer or None
+    # print "weight-------------", l['exposures'][0]['weight'] #Integer ?
+    # print "height-------------", l['exposures'][0]['height'] #Integer ?
+    # print "bmi----------------", l['exposures'][0]['bmi'] #Float / Integer ?
+    # print "years smoked-------", l['exposures'][0]['years_smoked'] # Float, maybe it is possible to convert it to int
+    # print "cigarretes per day-", l['exposures'][0]['cigarettes_per_day'] # Float or None
+    # print "alcohol history----", l['exposures'][0]['alcohol_history'] # String: 'yes' or 'no'
+    # print "alcohol intensity-",  l['exposures'][0]['alcohol_intensity'] #Integer ??
+
     try :
         store = True
         if len(l['exposures']) > 1 :
@@ -84,11 +105,24 @@ for l in j :
             else :
                 f.write("{}, ".format(str(l['diagnoses'][0]['age_at_diagnosis']/365)))
 
-            #Float. Days to death. None if still alive?
-            if l['diagnoses'][0]['days_to_death'] == None :
+            #Float. Days to death. None if still alive?	   	
+	    # JEC: seems that this field has been changed to be under  demographic key
+            # if l['diagnoses'][0]['days_to_death'] == None :
+	    try:
+	    	days_to_death = l['demographic']['days_to_death'] 
+	    except KeyError:
+	    	days_to_death = None
+	    	print "days_to_death not found in entry {}, set to null.".format(l['case_id'])
+	    # if l['demographic']['days_to_death'] == None :
+        #         f.write("NULL, ")
+        # else :
+	    # 	f.write("{}, ".format(str(int(l['diagnoses'][0]['days_to_death']))))
+	    # 	f.write("{}, ".format(str(int(l['demographic']['days_to_death']))))
+	    if days_to_death == None :
                 f.write("NULL, ")
             else :
-                f.write("{}, ".format(str(int(l['diagnoses'][0]['days_to_death']))))
+                # f.write("{}, ".format(str(int(l['diagnoses'][0]['days_to_death']))))
+                f.write("{}, ".format(str(int(l['demographic']['days_to_death']))))
 
             if l['demographic']['gender'] == None : #Either 'female' or 'male'
                 f.write("NULL, ")
